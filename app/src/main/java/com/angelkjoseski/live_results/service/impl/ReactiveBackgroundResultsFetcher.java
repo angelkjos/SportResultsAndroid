81c9f781d7e20713fa90a0d0edbb42ace8cf80b6
Angel Kjoseski <angel.koseski@gmail.com>
2017-04-03 23:35:47 +0200

    Show notifications when pooling for results

--- app/src/main/java/com/angelkjoseski/live_results/service/impl/ReactiveBackgroundResultsFetcher.java
+++ app/src/main/java/com/angelkjoseski/live_results/service/impl/ReactiveBackgroundResultsFetcher.java
@@ -4,6 +4,7 @@ import com.angelkjoseski.live_results.model.Fixture;
 import com.angelkjoseski.live_results.model.FixtureList;
 import com.angelkjoseski.live_results.service.BackgroundResultsFetcher;
 import com.angelkjoseski.live_results.service.FavouriteService;
+import com.angelkjoseski.live_results.service.NotificationService;
 import com.angelkjoseski.live_results.service.networking.ApiService;
 import com.angelkjoseski.live_results.util.rx_transformers.FillFixturesWithTeamDetailsTransformer;
 import com.angelkjoseski.live_results.util.rx_transformers.FixturesBasedOnFavouritesTransformer;
@@ -17,6 +18,7 @@ import javax.inject.Inject;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
@@ -29,11 +31,16 @@ public class ReactiveBackgroundResultsFetcher implements BackgroundResultsFetche
 
     private ApiService apiService;
     private FavouriteService favouriteService;
+    private NotificationService notificationService;
+
 
     @Inject
-    public ReactiveBackgroundResultsFetcher(ApiService apiService, FavouriteService favouriteService) {
+    public ReactiveBackgroundResultsFetcher(ApiService apiService,
+                                            FavouriteService favouriteService,
+                                            NotificationService notificationService) {
         this.apiService = apiService;
         this.favouriteService = favouriteService;
+        this.notificationService = notificationService;
     }
 
     @Override
@@ -77,6 +84,14 @@ public class ReactiveBackgroundResultsFetcher implements BackgroundResultsFetche
                             throw new Exception("No live fixtures right now.");
                         } else {
                             return false;
+                        }
+                    }
+                })
+                .doOnNext(new Consumer<List<Fixture>>() {
+                    @Override
+                    public void accept(List<Fixture> fixtures) throws Exception {
+                        for (Fixture fixture : fixtures) {
+                            notificationService.showNotificationForResult(fixture);
                         }
                     }
                 })

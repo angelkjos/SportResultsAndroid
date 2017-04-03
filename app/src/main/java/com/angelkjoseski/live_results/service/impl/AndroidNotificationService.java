bbb90feb5eebedef867b9386ce3e65aeb656d9a2
Angel Kjoseski <angel.koseski@gmail.com>
2017-04-04 00:17:00 +0200

    Add logic to not show notification for results which are already shown

--- app/src/main/java/com/angelkjoseski/live_results/service/impl/AndroidNotificationService.java
+++ app/src/main/java/com/angelkjoseski/live_results/service/impl/AndroidNotificationService.java
@@ -3,10 +3,13 @@ package com.angelkjoseski.live_results.service.impl;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.support.v4.app.NotificationCompat;
+import android.util.SparseBooleanArray;
 
 import com.angelkjoseski.live_results.R;
 import com.angelkjoseski.live_results.model.Fixture;
 import com.angelkjoseski.live_results.service.NotificationService;
+
+import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -18,10 +21,12 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 public class AndroidNotificationService implements NotificationService {
 
     private Context context;
+    private SparseBooleanArray alreadyShownNotifications; // Im-Memory storage of shown notifications
 
     @Inject
     public AndroidNotificationService(Context context) {
         this.context = context;
+        alreadyShownNotifications = new SparseBooleanArray();
     }
 
     @Override
@@ -32,10 +37,20 @@ public class AndroidNotificationService implements NotificationService {
                         .setContentTitle(fixture.getResult().getScoreString())
                         .setContentText(fixture.getTeamsString());
         // Sets an ID for the notification
-        int mNotificationId = (int) fixture.getFixtureId();
+        int mNotificationId = createNotificationId(fixture);
+        if (alreadyShownNotifications.get(mNotificationId, false)) {
+            return;
+        }
+        alreadyShownNotifications.put(mNotificationId, true);
         // Gets an instance of the NotificationManager service
         NotificationManager mNotifyMgr = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
         // Builds the notification and issues it.
         mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
+    private int createNotificationId(Fixture fixture) {
+        String fixtureString = String.format(Locale.getDefault(), "%d%s-%s",
+                fixture.getFixtureId(), fixture.getTeamsString(), fixture.getResult().getScoreString());
+        return fixtureString.hashCode();
     }
 }
